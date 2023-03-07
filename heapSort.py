d8da12846b10258a47ec8af422351a0654b6fc18
Michał Kopczewski <kopczewski.michal101@gmail.com>
2023-03-07 22:45:58 +0100

    heapSort z pomiarem czasu

--- heapSort.py
+++ heapSort.py
@@ -1,3 +1,5 @@
+from timeit import default_timer as timer
+import math
 # lewy syn A[i] jest w A[2i+1]
 # - prawy syn A[i] jest w A[2i+2]
 # - ojciec A[i] jest w A[⎣(i-1)/2⎦]
@@ -19,7 +21,7 @@ def heapify(A, heapSize, i):
 
 def buildHeap(A):
     heapSize = len(A)
-    k= int((len(A) - 2) / 2)  # ojciec ostatniego węzła
+    k = int((len(A) - 2) / 2)  # ojciec ostatniego węzła
     for i in range(k, -1, -1):
         heapify(A, heapSize, i)
     return A
@@ -35,6 +37,58 @@ def heapSort(A):
     return A
 
 
-list = [random.randint(1, 1000) for i in range(200)]
-B = heapSort(list)
-print(B.)
+# list = [random.randint(1, 1000) for i in range(200)]
+# B = heapSort(list)
+# print(B)
+
+
+def prettyPrint(n, time, divided):
+    z = 30
+    result = str(n) + "  " + str(time)
+    if len(result) < z:
+        result += (' ' * (z - len(result)))
+    print(result, divided)
+
+
+def pomiar_czasu_rosnące(nn):
+    for n in nn:
+        list = [i for i in range(n)]
+        start = timer()
+        heapSort(list)
+        stop = timer()
+        Tn = stop - start
+        Fn = n * math.log(n)
+        prettyPrint(n, Tn, Fn / Tn)
+
+def pomiar_czasu_identyczne(nn):
+    for n in nn:
+        list = [7 for i in range(n)]
+        start = timer()
+        heapSort(list)
+        stop = timer()
+        Tn = stop - start
+        Fn = n * math.log(n)
+        prettyPrint(n, Tn, Fn / Tn)
+
+def pomiar_czasu_malejące(nn):
+    for n in nn:
+        list = [i for i in range(n, -1, -1)]
+        start = timer()
+        heapSort(list)
+        stop = timer()
+        Tn = stop - start
+        Fn = n * math.log(n)
+        prettyPrint(n, Tn, Fn / Tn)
+
+
+def podsumowanieTestów(nn):
+    # nn = [100, 200, 500, 1000, 5000, 10000]
+    print("Poniżej przypadek idealny, tablica już posortowana")
+    pomiar_czasu_rosnące(nn)
+    print("\nPoniżej jeśli damy tablicę identycznych liczb")
+    pomiar_czasu_identyczne(nn)
+    print("\nPrzypadek tablicy wejściowej ułożonej malejąco")
+    pomiar_czasu_malejące(nn)
+
+
+podsumowanieTestów([100, 200, 500, 1000, 5000, 10000, 50000])
